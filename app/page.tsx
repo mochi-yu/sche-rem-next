a3bbfe70b2ad438503c43008c575707ab266b932
mochi-yu <mochi.2225.it@gmail.com>
2023-08-18 01:08:04 +0900

    fix: 不要なインポートを削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import { Typography } from '@mui/material'
-import Image from 'next/image'
-import { ScheRemHeader } from './ScheRemHeader'
 import { redirect } from 'next/navigation'
 
 export default function Home() {

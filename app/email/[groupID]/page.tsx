1bdb4d03daf7e3e3d790711743590b189f36d46c
mochi-yu <mochi.2225.it@gmail.com>
2023-08-18 23:59:43 +0900

    fix: 要素を左右中央に表示

--- app/email/[groupID]/page.tsx
+++ app/email/[groupID]/page.tsx
@@ -31,14 +31,21 @@ export default function EmailPage({params} : {params: {groupID: string}}) {
         <Container sx={{my: "20px", bgcolor: "white", py: "20px", px: "20px"}} maxWidth={false}>
           <Stack direction="row" alignItems="center">
             <Typography children={sampleData["groupName"]} variant="h3" sx={{mr: "50px"}} />
-            <Typography variant="h6">作成者：{sampleData["author"]}</Typography>
+            <Typography variant="h6">
+              作成者：{sampleData["author"]}
+            </Typography>
           </Stack>
 
-          <Typography variant="body1" sx={{py: "30px"}}>メールアドレスを入力してください。</Typography>
-          <Container sx={{alignItems: "center"}}>
-            <TextField id="outlined-basic" label="sample@example.com" variant="outlined"/><br></br>
-            <Button variant="contained" sx={{bgcolor: "#783200", mt: "20px"}}>次へ</Button>
-          </Container>
+          <Typography variant="body1" sx={{py: "30px", mx: "10px"}}>
+            メールアドレスを入力してください。
+          </Typography>
+
+          <Stack alignItems="center">
+            <TextField id="outlined-basic" label="sample@example.com" variant="outlined"/>
+            <Button variant="contained" sx={{bgcolor: "#783200", mt: "20px"}}>
+              次へ
+            </Button>
+          </Stack>
         </Container>
       </Container>
     </>
